2fb1a272d67b7174e489ea30fb36f4255403d176
Justin Singh <rod5547@gmail.com>
2025-01-08 06:49:05 -0800

    dice now changes randomly when roll is pressed

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,14 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let diceImages = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let dice1Index = Int.random(in: 0..<diceImages.count)
+        let dice2Index = Int.random(in: 0..<diceImages.count)
+        
+        diceImageView1.image = diceImages[dice1Index]
+        diceImageView2.image = diceImages[dice2Index]
+    }
+    
 }
 
