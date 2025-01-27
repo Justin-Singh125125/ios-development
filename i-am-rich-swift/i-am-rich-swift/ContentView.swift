a573de2364578610f738c31fa6c0412c3bab1428
Justin Singh <justin.singh125125@gmail.com>
2025-01-27 07:10:24 -0800

    added boilerplate

---
+++ i-am-rich-swift/i-am-rich-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  i-am-rich-swift
+//
+//  Created by Justin Singh on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ i-am-rich-swift/i-am-rich-swift/i_am_rich_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_rich_swiftApp.swift
+//  i-am-rich-swift
+//
+//  Created by Justin Singh on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct i_am_rich_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
